84f3f9358c46a4f2bd5437993b2497e91e5f8240
0binna-oss <richardokeiyi@gmail.com>
2025-03-11 22:25:49 +0100

    It works, kinda.

--- src/router.ts
+++ src/router.ts
@@ -1,4 +1,5 @@
 import {Procedure, Router} from "./types";
+import { withLogging } from "./middleware";
 
 export const createProcedure = <I, O>(procedure: Procedure<I, O>) => procedure;
 
@@ -8,4 +9,11 @@ export const createRouter = <T extends Router>(routes: T) => routes;
 export const router = createRouter({
     greet: createProcedure((input: { name: string }) => 'Hello, ${input.name}'),
     add: createProcedure((input: { a: number; b: number }) => input.a + input.b),
+    subtract: createProcedure((input: {a: number; b: number }) => input.a - input.b),
+    multiply: createProcedure((input: {a: number; b: number }) => input.a * input.b), 
+});
+
+export const routerWithLogging = createRouter({
+    greet: withLogging((input: {name: string}) => `Hello, ${input.name}`),
+    add: withLogging((input: {a: number; b: number}) => input.a + input.b),
 });
--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,14 @@
 import express from "express";
 import {Router} from "./types";
 import cors from "cors";
+import swaggerUi from "swagger-ui-express";
+import swaggerDocument from "../swagger.json";
 
 export const createServer = <T extends Router>(router: T) => {
     const app = express ();
     app.use(express.json());
     app.use(cors());
+    app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
     for (const Key in router) { 
         app.post(`/api/${Key}` , (req, res) => {
